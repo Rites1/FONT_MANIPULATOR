65362ea46ae2ff56150e857c8ab32b89e69ec2b5
Rites1 <103243296+Rites1@users.noreply.github.com>
2023-08-22 20:37:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', gotposes);
+}
+
+function modelloaded(){
+    console.log("poseNet is initilized");
+}
+
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
